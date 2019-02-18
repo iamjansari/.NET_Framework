6f64a489aa04bf65a549740d0aed29106e2cd88d
Manthan Jansari <manthanjansari12@gmail.com>
2019-02-18 13:21:54 -0500

    A

---
+++ Project_A/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace lab5A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.colourbtn = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colourbtn
+            // 
+            this.colourbtn.Location = new System.Drawing.Point(87, 38);
+            this.colourbtn.Name = "colourbtn";
+            this.colourbtn.Size = new System.Drawing.Size(84, 28);
+            this.colourbtn.TabIndex = 0;
+            this.colourbtn.Text = "colour";
+            this.colourbtn.UseVisualStyleBackColor = true;
+            this.colourbtn.Click += new System.EventHandler(this.colourbtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lab5A.Properties.Resources.Faucet;
+            this.pictureBox1.Location = new System.Drawing.Point(160, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(89, 68);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(210, 38);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(89, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 347);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.colourbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button colourbtn;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Project_A/Form1.cs
@@ -0,0 +1,136 @@
+﻿/*
+ * Name : Manthan Jansari 
+ * Student Id : 000741486
+ * Date: 07-01-2018
+ * purpose : The purpose of this assignment is to draw graphics using bultin function of .Net and 
+ *              more over this file shows the bucket is filling and we can control the flow of it
+ *              using trackbar and time is used to controll the timing of filling bucket
+ * Statement of Authorship : I have done this assignment by my own
+ * 
+ */
+
+
+using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5A
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;                         //Encapsulates a GDI+ drawing surface
+        private Pen p;                              //Pens are used to draw objects
+        private Color c = Color.Black;              //Represents a color, initially set to black
+        private Font f;                             //Defines a particular format for text, including font face, size, and style attributes
+        private SolidBrush b;                       //Brushes are used to fill graphics shapes
+        private int t = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            this.Paint += new PaintEventHandler(form1_Paint);  //Registers the Paint event handler
+        }
+
+        private void form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            g = e.Graphics;                          //Get the Graphics object from the PaintEventArgs
+            p = new Pen(Color.Black);                          //Create a new Pen using the current colour
+            f = new Font("Arial", 20);               //Create a new Font
+            b = new SolidBrush(c);                   //Create a new brush using the current colour
+
+            
+            g = this.CreateGraphics();               //Create a graphics object
+
+            g.DrawLine(p, 200, 200, 200, 300);  //Save the colour choice the user made
+            g.DrawLine(p, 400, 300, 200, 300);  //Save the colour choice the user made
+            g.DrawLine(p, 400, 300, 400, 200);  //Save the colour choice the user made
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void colourbtn_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = c;                //Display with the previous colour already chosen
+            colorDialog1.ShowDialog();             //Display the actual dialog box
+            c = colorDialog1.Color;
+            
+
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 100;
+            trackBar1.TickFrequency = 10;
+            
+            
+            if (trackBar1.Value == 1)
+            {
+                
+                timer1.Stop();
+            }
+            else
+            {
+                g.DrawLine(p, 230, 300, 230, 100);
+                g.DrawLine(p, 231, 300, 231, 100);
+                g.DrawLine(p, 232, 300, 232, 100);
+                g.DrawLine(p, 233, 300, 233, 100);
+                g.DrawLine(p, 234, 300, 234, 100);
+                g.DrawLine(p, 235, 300, 235, 100);
+                g.DrawLine(p, 236, 300, 236, 100);
+                g.DrawLine(p, 239, 300, 239, 100);
+                g.DrawLine(p, 238, 300, 238, 100);
+                g.DrawLine(p, 237, 300, 237, 100);
+
+                timer1.Start();
+                timer1.Interval = 101 - trackBar1.Value;
+            }
+
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            p = new Pen(c);                          //Create a new Pen using the current colour
+
+            if (t != 90)
+            {
+               
+                g.DrawLine(p, 400, 300 - t, 200, 300 - t);  //Save the colour choice the user made
+                t++;
+            }
+
+            else
+            {
+                timer1.Stop();
+                Refresh();
+                t = 0;
+                trackBar1.Value = 1;
+            }
+        }
+    }
+}
